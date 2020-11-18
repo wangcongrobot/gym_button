8806abfb96d3763305bde125db8acd702aedf5dc
Cong Wang <wangcongrobot@gmail.com>
2020-11-18 10:03:09 +0000

    random button

--- gym/envs/mjrl/dkitty_button_random.py
+++ gym/envs/mjrl/dkitty_button_random.py
@@ -118,13 +118,14 @@ class ButtonRandomEnv(mujoco_env.MujocoEnv, utils.EzPickle):
             self.data.qpos[2] = np.random.uniform(-1.0, 1.0)
             self.data.qpos[3] = np.random.uniform(-1.0, 1.0)
             self.data.qpos[4] = np.random.uniform(-1.0, 1.0)
-
+            # button position
+            self.data.qpos[5] = np.random.uniform(-0.1, 0.1)
             # choose button initial state randomly
             self.data.qpos[-1] = random.choice([0.4,1.7])
             if self.data.qpos[-1] == 0.4: # on
                 self.button_state = 0
             if self.data.qpos[-1] == 1.7: # off
-                self.button_state = 1     
+                self.button_state = 1
 
             for _ in range(20):
                 self.sim.step()
